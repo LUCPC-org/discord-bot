545b48012c6a4a75a5f233698b7461d491cad614
cameron <me@cameron.rs>
2024-08-28 21:41:19 -0400

    Update database schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,20 @@
-CREATE TABLE IF NOT EXISTS leaderboard (
+/* This entry is populated for every user who signs up for the leaderboard */
+CREATE TABLE IF NOT EXISTS leaderboard_entry (
     discord_id TEXT PRIMARY KEY NOT NULL,
     kattis_username TEXT NOT NULL,
     original_points INTEGER NOT NULL,
     current_points INTEGER NOT NULL
+)
+
+/* This table is used if we want to make a graph for the user that will show their progress throughout the semester */
+CREATE TABLE IF NOT EXISTS score_snapshot (
+    score INTEGER NOT NULL,
+    discord_id TEXT NOT NULL,
+
+    /*
+    * SQLite does not have a date type, so we will store the date as a string 
+    * Date format: YYYY-MM-DD 
+    */
+    date TEXT NOT NULL,
+    FOREIGN KEY(discord_id) REFERENCES leaderboard_entry(discord_id)
 )
